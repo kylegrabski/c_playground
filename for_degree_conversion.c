b54320384398bfbef6d94cc83a1c94db4c3a28cd
kylegrabski <kyle.grabski@gmail.com>
2024-04-13 12:14:16 -0700

    what for??

---
+++ for_degree_conversion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* print Farenheit-Celsius table */
+main() 
+{
+    int fahr;
+
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+
+    printf("\nThe Inverse\n");
+
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+    
+}
